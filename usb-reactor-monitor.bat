3ef31a654b16974c98e1b2ac6d1a98fac4f9e283
Jeff Anderson <jeff.anderson@digecor.com>
2019-10-18 15:34:54 -0600

    Put logs in subdir

--- usb-reactor-monitor.bat
+++ usb-reactor-monitor.bat
@@ -1,3 +1,3 @@
 REM Restart USB reactor monitor in the background and log output.
-start /B python .\usb-reactor-monitor.py > .\usb-reactor-monitor.log
+start /B python .\usb-reactor-monitor.py > .\log\usb-reactor-monitor.log
 
--- usb-reactor.bat
+++ usb-reactor.bat
@@ -1,3 +1,3 @@
 REM Start USB reactor in the background and log output.
-start /B python .\usb-reactor.py --boot > .\usb-reactor.log
+start /B python .\usb-reactor.py --boot > .\log\usb-reactor.log
 
